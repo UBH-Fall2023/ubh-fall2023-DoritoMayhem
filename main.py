18c2f974355c0949d08422e872b6ad2c70244a36
DoritoMayhem <117592689+DoritoMayhem@users.noreply.github.com>
2023-11-04 16:07:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,43 +7,41 @@ py.display.set_caption("My Game")
 
 def main():
     py.init() 
-  
     clock = py.time.Clock() 
     
     FrameHeight = 600
     FrameWidth = 1000
-    
-    # PYGAME FRAME WINDOW 
+    scroll = 0
+
     py.display.set_caption("My Game") 
-    screen = py.display.set_mode((FrameWidth, FrameHeight)) 
-    
-    # IMAGE 
+    screen = py.display.set_mode((FrameWidth, FrameHeight))  
     bg = py.image.load("./images/space-background.jpg").convert() 
 
-    scroll = 0
+    
     tiles = math.ceil(FrameWidth / bg.get_width()) + 1
     
     # MAIN LOOP 
     while True: 
         # clock for the speed of the scrolling
         clock.tick(60) 
+
+        # background stuff
         i = 0
         while(i < tiles): 
             screen.blit(bg, (bg.get_width()*i + scroll, 0)) 
             i += 1
-        # FRAME FOR SCROLLING 
+        # This also affects the speed of the scrolling
         scroll -= 3
-    
-        # RESET THE SCROLL FRAME 
         if abs(scroll) > bg.get_width(): 
             scroll = 0
-        # CLOSINF THE FRAME OF SCROLLING 
+        
+
+        # Close the game
         for event in py.event.get(): 
             if event.type == py.QUIT: 
                 quit() 
     
         py.display.update() 
-    
     py.quit() 
 
 def background(bg_rect, screen_rect):
