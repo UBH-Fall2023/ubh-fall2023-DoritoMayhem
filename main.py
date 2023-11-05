ca5b1ce9d81ecfb3a78ea3a9830e43690f2be727
DoritoMayhem <117592689+DoritoMayhem@users.noreply.github.com>
2023-11-05 01:20:54 -0500

    final

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import pygame as py
 import math
 FrameHeight = 600
 FrameWidth = 1000
+high = [0]
 
 class Player(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = py.image.load("images/spaceship.png").convert_alpha()
         self.rect = self.image.get_rect()
-    
+        self.lives = 3
+
     def update(self, keys):
         if keys[py.K_UP] and self.rect.y >0:
             self.rect.y -= 5
@@ -24,18 +26,24 @@ class Score(object):
     def __init__(self):
         self.white = 255,255,255
         self.count = 0
+        self.high = high[0]
+        self.lifecount = 3
         self.font = py.font.SysFont("comicsans",20, True , True)
         self.text = self.font.render("Score : "+str(self.count),1,self.white)
+        self.live = self.font.render("Lives:"+str(self.lifecount),1,self.white)
+        self.highscore = self.font.render("High Score:"+str(self.high),1,self.white)
 
     def show_score(self, screen):
         screen.blit(self.text, (0 ,0))
-
+        screen.blit(self.live, (0,100))
+        screen.blit(self.highscore, (0,50))
     def score_up(self):
         self.count += 1
         self.text = self.font.render("Score : "+str(self.count),1,self.white)
+    def life_down(self):
+        self.lifecount -= .5
+        self.live = self.font.render("Lives:"+str(int(self.lifecount)),1,self.white )
 
-def hit():
-   pass
 
 def main():
     py.init()
@@ -49,26 +57,33 @@ def main():
     scroll = 0
     score = Score()
     maxlasers =6
-    asteroids_to_remove = []
-
-    
+    font = py.font.SysFont("comicsans",20, True , True)
+    txt = font.render("Game Over\nPress r to restart",1,(255,255,255))
     py.display.set_caption("My Game")
     screen = py.display.set_mode((FrameWidth, FrameHeight))
     bg = py.image.load("./images/space-background.jpg").convert()
     laserImage = py.image.load("./images/laser.jpg").convert_alpha()
-    asteroidImage = py.image.load("./images/medium rock.png").convert_alpha()
-    
+    laserMask = py.mask.from_surface(py.image.load("./images/laser.jpg").convert_alpha())
+    asteroidImages = [py.image.load("./images/small rock.png").convert_alpha(),
+                                                                    py.image.load("./images/medium rock.png").convert_alpha(),
+                                                                    py.image.load("./images/large rock.png").convert_alpha()]
+    asteroid_masks = [py.mask.from_surface(image) for image in asteroidImages]
     player = Player()
+    player_mask = py.mask.from_surface(player.image)
     tiles = math.ceil(FrameWidth / bg.get_width()) + 1
     asteroid_add_timer = py.time.get_ticks()
-
+    game_over = False
+    asteroids_to_remove = []
     # MAIN LOOP
-    while True:
+    while not game_over:
         # frames per second
         clock.tick(60)
         currentTime = py.time.get_ticks()
         time_elapsed = currentTime - asteroid_add_timer
-
+        if player.lives == 0:
+            if score.count > score.high:
+                high[0] = score.count
+            game_over = True
         # background stuff
         i = 0
         while i < tiles:
@@ -78,7 +93,6 @@ def main():
         scroll -= 3
         if abs(scroll) > bg.get_width():
             scroll = 0
-
         # player handling
         keys = py.key.get_pressed()
         player.update(keys)
@@ -87,9 +101,16 @@ def main():
         # Update asteroids and check for collisions
         if time_elapsed >= asteroid_add_interval:
             for _ in range(2):
-                asteroid = py.Rect(random.randint(FrameWidth, FrameWidth + 100),
-                                   random.randint(0, FrameHeight - 20), 128, 128)
-                asteroids.append(asteroid)
+                integer = random.randint(0, 2)
+                image = asteroidImages[integer]
+                mask = asteroid_masks[integer]
+                asteroid = image.get_rect()
+                asteroid.x = random.randint(FrameWidth, FrameWidth + 100)
+                asteroid.y = random.randint(0, FrameHeight - 20)
+                asteroids.append((asteroid, image, mask))
+                """asteroid = py.Rect(random.randint(FrameWidth, FrameWidth + 100),
+                                   random.randint(0, FrameHeight - 20), width, height )"""
+                asteroids.append((asteroid,image,mask))
 
             # Update the asteroid_add_timer
             asteroid_add_timer = currentTime
@@ -102,44 +123,43 @@ def main():
             if event.type == py.KEYDOWN and event.key == py.K_SPACE and len(lasers) <maxlasers:
                 laser = py.Rect(int(player.rect.x) + 100, int(player.rect.y) + 50, 10, 5)
                 lasers.append(laser)
-        # Detects for collisions and causes its affects
-        for asteroid in asteroids:
-            if player.rect.colliderect(asteroid):
-                ...
-            for laser in lasers:
-                if laser.colliderect(asteroid):
-                    asteroids_to_remove.append(asteroid)
-                    lasers.remove(laser)
-                    score.score_up()
-        for asteroid in asteroids_to_remove:
-            if asteroid in asteroids:
-                asteroids.remove(asteroid)
-       
+        
+    
+       # Detects for collisions and causes its affects
         for laser in lasers:
             laser.x += laserVelocity
-        for laser in lasers:
             screen.blit(laserImage, laser.topleft)
-        for asteroid in asteroids:
-            asteroid.x -= asteroidVelocity
-        for asteroid in asteroids:
-            screen.blit(asteroidImage, asteroid.topleft)
-
-        for asteroid in asteroids:
-            if asteroid.right < 0:
-                asteroids.remove(asteroid)
-        for laser in lasers:
             if laser.left > 950:
                 lasers.remove(laser)
+        for asteroid,image,mask in asteroids:
+            asteroid.x -= asteroidVelocity
+            screen.blit(image, asteroid.topleft)
+            if asteroid.right < 0:
+                asteroids.remove((asteroid,image,mask))
+            if mask.overlap_area(player_mask, (player.rect.x - asteroid.x, player.rect.y - asteroid.y)) > 0:
+                asteroids.remove((asteroid,image,mask))
+                player.lives -=.5
+                score.life_down()
+                
+            for laser in lasers:
+                if mask.overlap_area(laserMask, (laser.x - asteroid.x, laser.y - asteroid.y)) > 0:
+                    asteroids_to_remove.append((asteroid, image, mask))
+                    lasers.remove(laser)
+                    score.score_up()
+        for asteroid_data in asteroids_to_remove:
+            if asteroid_data in asteroids:
+                asteroids.remove(asteroid_data)
         score.show_score(screen)
         py.display.update()
-
-
-   # makes asteroids go boom
-def hit():
-   pass
-
-
-  # laser from spaceship and how it acts (travel route)
+    while game_over:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                py.quit()
+            if event.type == py.KEYDOWN and event.key == py.K_r:
+                main()  # Restart the game
+        screen.blit(txt, (350, 250))
+        py.display.update()
+   
 
 
 
